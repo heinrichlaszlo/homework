58f2e40d8e972085920bb5efc24c222791bb7d31
heinrichlaszlo <43448196+heinrichlaszlo@users.noreply.github.com>
2024-02-14 20:49:45 +0100

    feat: create address.java model

---
+++ homework/src/main/java/com/kesmarki/homework/model/Address.java
@@ -0,0 +1,34 @@
+package com.kesmarki.homework.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private AddressType type;
+
+    private String address;
+
+    @ManyToOne
+    @JoinColumn(name = "person_id")
+    @JsonIgnore
+    private Person person;
+
+   @OneToMany(mappedBy = "address")
+   private List<Contact> contacts = new ArrayList<>();
+}
