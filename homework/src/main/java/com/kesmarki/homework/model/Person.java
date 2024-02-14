0fc7556fa0fa2463807747da8379bf4a767c262f
heinrichlaszlo <43448196+heinrichlaszlo@users.noreply.github.com>
2024-02-14 20:50:18 +0100

    feat: create person entity

---
+++ homework/src/main/java/com/kesmarki/homework/model/Person.java
@@ -0,0 +1,26 @@
+package com.kesmarki.homework.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "person")
+    private List<Address> addresses;
+}
